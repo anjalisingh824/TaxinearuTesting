f611dc9809d2910f330e0ecc80a8452f071c15df
anjalisingh824 <anjali.singh824@gmail.com>
2016-11-14 18:14:52 +0530

    Add files via upload

---
+++ src/main/java/Utils/Constant.java
@@ -0,0 +1,25 @@
+package Utils;
+
+public class Constant {
+	public static final String FILE_FULL_PATH = "/home/smart/workspace/taxinearTesting/src/main/resources/Taxinearu.xlsx";
+	public static final String FILE_PATH = "src/main/resources/";
+	public static final String FILE_NAME = "Taxinearu.xlsx";
+	public static final String SHEET_NAME = "Sheet1";
+	public static final String URL = "http://138.68.137.97:8080/tnu/home/home.jsp";
+	public static final String BASE_URL = "http://138.68.137.97:8080/";
+	public static final String TITLE = "Taxinearu - Search, Book & Go.";
+	public static final String PASSWORD_REQUEST = "Your request to change your password has been sent to your email.";
+	public static final String CHANGEPASSWORDE_MSG = "Password should be 7 characters long, containing alphabetic, numeric and one capital letter, password cannot contain any special characters(~!@#$%^&*_+=|(){}[]:;<>,./?)";
+	public static final String PASSWORD_MISMATCH_ERROR = "New password and confirm new password mismatch.";
+	public static final String PASSWORD_CHANGED_SUCCESSMSG = "Your password has been updated.";
+	public static final String PRIMARY_EMAIL = "testing3492email@gmail.com";
+	public static final String PRIMARY_EMAIL_PASSWORD = "8306188737";
+	public static final String ALTERNATE_EMAIL = "anjali.smartsensesolutions@gmail.com";
+	public static final String ALTERNATE_EMAIL_PASSWORD = "smartAnjali";
+	public static final String CONNECTION_EMAIL = "imap.gmail.com";
+    public static final String SECURITY_QUESTION1="bangalore";
+    public static final String SECURITY_QUESTION2="anu";
+    public static final String SECURITY_CHANGEPASSERROR="Minimum 7 characters at least 1 Uppercase, 1 Lowercase, 1 Number and No Special Characters allowed";
+    public static final String PASSWORD_NOT_MATCH="Password does not match";
+
+}
